bd0c26bc00c383462e51309641ce29142bcdf030
Rhishav Lamichhane <110982820+SelAshyn@users.noreply.github.com>
2025-10-18 06:50:38 +0545

    Deleting Unwanted folders

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HealthcareHeroSections from ".//components/Info"
 import React from "react";
 import { About } from ".//components/About"
 import { Services } from ".//components/Services"
-import Hero from ".//components/Comp2/page"
+import Hero from "./components/Count"
 
 export default function Home() {
   return (
